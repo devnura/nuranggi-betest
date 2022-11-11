db8673bbe6dc95c7fec8255f032f5bf1d8f89910
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-11-12 04:01:51 +0700

    Update

--- UserService/index.js
+++ UserService/index.js
@@ -7,7 +7,7 @@ const express = require('express')
 const app = express()
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const port = 3002
+const port = process.env.APP_PORT;
 
 // const mongoose = require('mongoose');
 const mongoose = require('./infrastructure/database/database.mongo');
@@ -36,48 +36,6 @@ app.use((req, res, next) => {
 })
 
 router.use('/user', userRoute)
-
-// app.post("/insert", async (req, res) => {
-//   const foodName = req.body.foodName;
-//   const days = req.body.days;
-//   const food = new FoodModel({ foodName: foodName, daysSinceIAte: days });
-//   try {
-//     await food.save();
-//     res.send("data inserted");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// app.put("/update", async (req, res) => {
-//   const newFoodName = req.body.newFoodName;
-//   const id = req.body.id;
-
-//   try {
-//     await FoodModel.findById(id, (err, updatedFood) => {
-//       updatedFood.foodName = newFoodName;
-//       updatedFood.save();
-//       res.send("update");
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// app.get("/read", async (req, res) => {
-//   FoodModel.find({}, (err, result) => {
-//     if (err) {
-//       res.send(err);
-//     }
-//     res.send(result);
-//   });
-// });
-
-// app.delete("/delete/:id", async (req, res) => {
-//   const id = req.params.id;
-//   await FoodModel.findByIdAndRemove(id).exec();
-//   res.send("deleted");
-// });
 
 app.listen(port, () => {
   console.log(`User Services app listening on port ${port}`)
